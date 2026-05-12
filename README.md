b27f799a14ae7bb95e0565e05da98b2d8ce49916
Frederico994 <a14620@oficina.pt>
2026-05-12 16:04:20 +0000

    Projeto

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # oficina-reconhecimento-facial-nodejs
 Projeto de Reconhecimento facial 
+
+# supabase 
+api-url : https://zrzjabpqftgzxnunlprq.supabase.co/rest/v1/
+
+
+anon public: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpyemphYnBxZnRnenhudW5scHJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1ODQ3MDUsImV4cCI6MjA5NDE2MDcwNX0.AmxwQ-VE3zLL64amhbtwU8A3Jt3fd_2iPfV1b0CuFS4
+
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
+
+
+const { createClient } = require("@supabase/supabase-js");
 
 const app = express();
 
@@ -8,13 +12,15 @@ const app = express();
 
 app.use(cors());
 
+// SERVIR PUBLIC PARA O ACESSO PUBLICO
 app.use(express.static("public"));
 
+// SERVIR MODELS (MUITO IMPORTANTE) PARA O ACESSO REFERENCIAL
+app.use('/models', express.static('models')); 
 
 
 
-
-
+/*
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
@@ -25,13 +31,102 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+*/
+
+const supabase = createClient("https://zrzjabpqftgzxnunlprq.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpyemphYnBxZnRnenhudW5scHJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1ODQ3MDUsImV4cCI6MjA5NDE2MDcwNX0.AmxwQ-VE3zLL64amhbtwU8A3Jt3fd_2iPfV1b0CuFS4");
+
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage });
+
+app.post("/upload", upload.single("photo"), async (req, res) => {
+
+  try {
+
+    const emotion = req.body.emotion;
+    const nameofstudents = req.body.name;
+
+    const file = req.file;
+
+    const fileName = Date.now() + ".jpg";
+
+    // Upload para storage no supabase
+
+    const { data, error } =
+
+      await supabase.storage
+
+        .from("photos")
+
+        .upload(fileName,
+
+          file.buffer,
+
+          {
+
+            contentType: "image/jpeg"
+
+          });
 
 
+    if (error)
+
+      throw error;
+
+    // Ligar URL pública
+
+    const { data: publicUrl } =
+
+      supabase.storage
+
+        .from("photos")
+
+        .getPublicUrl(fileName);
+
+    // Guardar na tabela
+
+    await supabase
+
+      .from("students")
+
+      .insert([{
+
+        name: nameofstudents,
+
+        emotion: emotion,
+
+        photo_url:
+
+          publicUrl.publicUrl
+
+      }]);
+
+    res.json({
+
+      message: "Guardado com sucesso"
+
+    });
+
+  }
+
+  catch (err) {
+
+    console.log(err);
+
+    res.status(500).json({
+
+      error: err.message
+
+    });
+
+  }
+
+});
 
 
 
 // endpoint para guardar um ficheiro/foto no local que foi 
-// designado na criação de 'storage' e depois como upload através de multer
+/* designado na criação de 'storage' e depois como upload através de multer
 app.post("/upload", upload.single("photo"), (req, res) => {
   console.log("Foto recebida:", req.file.filename);
   res.json({
@@ -39,7 +134,7 @@ app.post("/upload", upload.single("photo"), (req, res) => {
     file: req.file.filename
   });
 
-});
+});*/
 
 
 app.listen(3000, () => {
